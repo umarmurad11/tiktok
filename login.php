7a439a69c3e0cb6664bb4a7b001a1466c2b7526e
umarmurad11 <umarmurad.11@gmail.com>
2025-01-13 01:31:53 +0000

    Update login.php

--- login.php
+++ login.php
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (password_verify($password, $user['password'])) {
             session_start();
             $_SESSION['user_id'] = $user['id'];
-            header("Location: home.php");
+            header("Location: index.php");
             exit();
         } else {
             echo "<script>alert('Invalid password.');</script>";
